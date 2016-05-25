9929c50d24173aeafc96715f84d656f928f6a07d
dj0wns <jones277@ouchiebooboo.llnl.gov>
2016-05-25 16:37:00 -0700

    Theoretically working

--- beam/beam.c
+++ beam/beam.c
@@ -1,44 +1,27 @@
-
-
-
+#define l b[1][i]
+#define C case 
 main(a,b,c,d,x,y,z,p,i)char *b[];{
-	for(c=y=p=z=x=i=0;
-			b[0][i]=='\n'?
-			x=0,
-			y++,
-			z<2
-			:z<2;
-			i++,x++){
-		printf("%i,%i\t",b[0][i],i);
-		switch(b[0][i]){
-			case '0':
-			case 'v':
-			case '^':
-			case '<':
-			case '>':
-				if(!(p>30)) p=b[0][i];
-				c=x,d=y,z++;
-
+	for(c=y=p=z=i=0,x=1;z<2 && (l=='\n'?x=0,y++,1:1);i++,x++){
+		switch(l){
+		C'0':C'v':C'^':C'<':C'>':if(p<50)p=l;if(!z)c=x,d=y;z++;
 		}
 	}
-	--i;
-	printf("%i,%i,%i,%i,%i,%i,%i,%i,%i",a,b,c,d,x,y,z,p,i);
+	i--;x--;
+	z=0;
 	switch(p){
-		case 'v':
-			z=(c==x)*(b[1][i]=='0');
+		C'v':
+			z=(c==x)*(l=='0');
 			break;
-		case '^':
-			z=(c==x)*(b[1][i]==p);
+		C'^':
+			z=(c==x)*(l==p);
 			break;
-		case '<':
-			z=(d==y)*(b[1][i]==p);
+		C'<':
+			z=(d==y)*(l==p);
 			break;
-		case '>':
-			z=(d==y)*(b[1][i]=='0');
+		C'>':
+			z=(d==y)*(l=='0');
 			break;
 
 	}
-
-	/*printf("%i,%i,%i,%i,%i,%i,%i,%i",a,b,c,d,x,y,z,p);*/
 	return z;
 }
